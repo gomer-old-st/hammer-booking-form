39bf9a34069e393a693284a54191f8a818f7476a
Gomer Abesamis <gomer@old.st>
2022-04-25 17:02:19 +0800

    Fixing integrations

--- calendar-script.js
+++ calendar-script.js
@@ -274,7 +274,7 @@ var selectedButton = null;
 
 	function selectSched(e) {
 		console.log('UHGDSOUDSGD');
-		console.log.(e);
+		console.log(e);
 		selectedTime = e.target.value;
 		//document.querySelector('#chosenSChed').value = selectedTime;
 		localStorage.setItem('schedDateTime', selectedTime);
