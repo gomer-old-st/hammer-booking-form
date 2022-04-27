2a7c928587cbe17999ac887740cf5cdadcc8d0e5
Gomer Abesamis <gomer@old.st>
2022-04-27 15:36:28 +0800

    update calendar

--- calendar-script.js
+++ calendar-script.js
@@ -105,7 +105,7 @@ const telehealthServices = [
 		
 		var tempDate = this.current;
 		
-		console.log('B');
+		console.log('C');
 		
 		var date = tempDate.toDate();
 		month = '' + (date.getMonth() + 1),
