7e147ae509603bd6d56dad9f661e9add3c5ff151
Gomer Abesamis <gomer@old.st>
2022-05-10 21:54:56 +0800

    update

--- calendar-script.js
+++ calendar-script.js
@@ -636,6 +636,7 @@ var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oc
 	}
   
 	Calendar.prototype.callServices = function(date) {
+		console.log('A1');
 		$.ajax({
 			type: 'GET',
 			url: 'https://rld1z7xwl9.execute-api.us-west-1.amazonaws.com/dev/calendar',
