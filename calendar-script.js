d095e0704daee11a212e28752a5009ccd890aba2
Gomer Abesamis <gomer@old.st>
2022-05-11 15:31:03 +0800

    update

--- calendar-script.js
+++ calendar-script.js
@@ -69,7 +69,7 @@ var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oc
 		
 		var tempDate = this.current;
 		
-		console.log('H');
+		console.log('F');
 		
 		var date = tempDate.toDate();
 		month = '' + (date.getMonth() + 1),
@@ -478,6 +478,9 @@ var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oc
 					homevisitSched.appendChild(btn);
 				}
 			} else if (ev.colorId === 1) {
+				console.log('HI');
+				console.log(ev.clinicNo);
+				console.log(selected);
 				if (!done2.includes(time1) && ev.clinicNo === selected) {
 					done2.push(time1);
 					btn = this.createSchedButton('clinicvisitSched', ev);
