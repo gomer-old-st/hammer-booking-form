e76c7eb728c61e8c43a6e2ef226624d32d2be0d3
Gomer Abesamis <gomer@old.st>
2022-05-16 21:24:09 +0800

    update

--- calendar-script.js
+++ calendar-script.js
@@ -65,7 +65,7 @@ $.ajax({
 
 	function Calendar(selector, events) {
 		localInstance = this;
-		console.log('BB')
+		console.log('BC')
 		var checkbox1 = document.querySelector('#homevisit');
 		var checkbox2 = document.querySelector('#clinicvisit');
 		var checkbox3 = document.querySelector('#telehealth');
@@ -368,6 +368,7 @@ $.ajax({
 
 			generalEvents.forEach(function(ev) {
 				var evSpan = createElement('span', ev.color);
+				console.log(ev);
 				element.appendChild(evSpan);
 			});
 		}
